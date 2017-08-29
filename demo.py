017ce7c272a62d1ed5e33bb9ec13aa2b106110ee
sca teu <scateu@scadeMacBook-Pro.local>
2017-08-30 00:27:37 +0800

    .

---
+++ demo.py
@@ -0,0 +1,37 @@
+#-*- coding:utf-8
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import random
+
+a = [
+        "自主",
+        "可控",
+        "物联网",
+        "网联",
+        "智能",
+        "大数据", 
+        "可视化",
+        "云计算",
+        "Android",
+        "威胁情报",
+        "感知",
+        "系统",
+        "人工智能",
+        "机器学习",
+        "跨平台",
+        "安全水位",
+        "透明",
+        "计算",
+        "等保",
+        "数据泄漏",
+        "不忘初心",
+        "方得始终",
+        "价值",
+        "让世界变得更美好",
+        "海量",
+        "卷积神经网络",
+        "扁平",
+        "民主设计",
+        ]
+print("%s%s"%(random.choice(a),random.choice(a)))
